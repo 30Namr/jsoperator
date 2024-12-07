6edfad7e76344a8a175272171bd4957c58be3dd3
30Namr <maurya.namrata88@gmail.com>
2024-12-07 15:12:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a = 5;
+var b = 10;
+var c=a+b;
+var d=a-b;
+var e=a*b;
+var f=a/b;
+document.write(`Add ${a} & ${b} is ${c} <br>`);
+document.write(`Sub ${a} & ${b} is ${d} <br>`);
+document.write(`Mult ${a} & ${b} is ${e} <br>`);
+document.write(`Div ${a} & ${b} is ${f} <br>`);
+
+
